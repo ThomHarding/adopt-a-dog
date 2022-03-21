8c3dd0527d5d4c1e59433de77447081dd3896106
THOMAS HARDING <hardingt604@gmail.com>
2022-03-21 13:54:52 -0600

    links properly to details page which itself fetches specific dogs properly

--- detail/detail.js
+++ detail/detail.js
@@ -3,7 +3,14 @@ import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
+window.addEventListener('load', async() => {
 // on load
 // get the id from URL
+    const data = new URLSearchParams(window.location.search);
+    let id = data.get('id');
 // use the id to fetch the dog
+    let dog = await getDog(id);
 // render and append this dog's details to the container
+    let dogDetailEl = renderDogDetail(dog);
+    dogDetailContainer.append(dogDetailEl);
+});
